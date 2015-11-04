c81b5750a66dcef5e49ec12432e4aefac3bfee5c
Super Fake <superfake@Supers-MacBook-Pro.local>
2015-11-05 00:22:40 +0700

    Initial Commit

---
+++ thaoluan1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  thaoluan1
+//
+//  Created by Super Fake on 11/5/15.
+//  Copyright (c) 2015 Super Fake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ thaoluan1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  thaoluan1
+//
+//  Created by Super Fake on 11/5/15.
+//  Copyright (c) 2015 Super Fake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
